1b2b56c31118e1018fa17079249d60f006c8ecb6
unknown <deya1907049@stud.kuet.ac.bd>
2023-03-26 15:44:55 +0600

    changed in second

---
+++ app/src/main/java/com/example/eggtimer/demo2.java
@@ -0,0 +1,37 @@
+package com.example.eggtimer;
+
+public class demo2 {
+    private int income;
+    private String f_name;
+    private String m_name;
+
+    public demo2(int income, String f_name, String m_name) {
+        this.income = income;
+        this.f_name = f_name;
+        this.m_name = m_name;
+    }
+
+    public int getIncome() {
+        return income;
+    }
+
+    public void setIncome(int income) {
+        this.income = income;
+    }
+
+    public String getF_name() {
+        return f_name;
+    }
+
+    public void setF_name(String f_name) {
+        this.f_name = f_name;
+    }
+
+    public String getM_name() {
+        return m_name;
+    }
+
+    public void setM_name(String m_name) {
+        this.m_name = m_name;
+    }
+}
